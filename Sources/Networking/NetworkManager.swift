78a6def91437ce5b48df15d7f6e2afedc3a65274
wasif2525 <Bhuiyan.Wasif@techconsulting.tech>
2024-12-05 21:29:01 -0500

    first commit

---
+++ Sources/Networking/NetworkManager.swift
@@ -0,0 +1,46 @@
+//
+//  NetworkManager.swift
+//  NetworkingFetchingData
+//
+//  Created by Bhuiyan Wasif on 11/21/24.
+
+import Foundation
+
+public protocol NetworkingManagerProtocol {
+    func fetchData<T:Decodable>(url: String, modelType: T.Type) async throws -> T
+}
+
+public class NetworkManager{
+    let urlSession: URLSession
+    
+    public init(urlSession: URLSession = URLSession.shared) {
+        self.urlSession = urlSession
+    }
+}
+
+extension NetworkManager: NetworkingManagerProtocol {
+    public func fetchData<T>(url: String, modelType: T.Type) async throws -> T where T : Decodable {
+        guard let url = URL(string: url) else {
+            throw NetworkError.invalidURL
+        }
+        do{
+            let (data, response) = try await self.urlSession.data(from: url)
+            guard let response = response as? HTTPURLResponse else {
+                throw NetworkError.invalidResponse
+            }
+            guard (200...299).contains(response.statusCode) else {
+                throw NetworkError.invalidHTTPStatusCode
+            }
+            
+            let parsedData = try JSONDecoder().decode(modelType, from: data)
+            return parsedData
+            
+        }catch{
+            print(error.localizedDescription)
+            throw error
+        }
+        
+    }
+    
+    
+}
